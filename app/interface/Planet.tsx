b813ef1cc1bacdcfb1c005677bcf40a1ef2aed13
Nichola Allen <nicholakallen@Nicholas-iMac.attlocal.net>
2024-11-27 21:21:33 -0500

    updated planet.tsx file

--- app/interface/Planet.tsx
+++ app/interface/Planet.tsx
@@ -1,15 +1,17 @@
 
 export interface Planet {
-    name: string;
-    [key: string]: string | number | undefined;
-  }
-  
+   name: string;
+   description?: string; 
+   diameter?: number; 
+   [key: string]: string | number | undefined;
+ }
+ 
 export interface HeaderComponentProps {
-   data?: Planet[]; 
-   onDataChange: (value: string) => void;
+  data?: Planet[]; 
+  onDataChange: (value: string) => void;
 }
 
 export interface PlanetComponentProps {
-    data?: Planet[]; 
-    planet: Planet;
- }
+   data?: Planet[]; 
+   planet: Planet;
+}
