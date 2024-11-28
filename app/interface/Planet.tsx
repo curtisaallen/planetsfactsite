55a630cfc03563c4fbe34cc2652a88db9088a34b
Nichola Allen <nicholakallen@Nicholas-iMac.attlocal.net>
2024-11-27 21:40:55 -0500

    updated interface

--- app/interface/Planet.tsx
+++ app/interface/Planet.tsx
@@ -1,17 +1,28 @@
-
 export interface Planet {
    name: string;
-   description?: string; 
-   diameter?: number; 
-   [key: string]: string | number | undefined;
+   description?: string;
+   diameter?: number;
+   overview?: {
+     content: string;
+     source: string;
+   };
+   structure?: {
+     content: string;
+     source: string;
+   };
+   geology?: {
+     content: string;
+     source: string;
+   };
+   rotation?: string;
+   revolution?: string;
+   radius?: string;
+   temperature?: string;
+   images?: {
+     planet: string;
+     internal: string;
+     geology: string;
+   };
+   [key: string]: string | number | undefined | Record<string, unknown>;
  }
  
-export interface HeaderComponentProps {
-  data?: Planet[]; 
-  onDataChange: (value: string) => void;
-}
-
-export interface PlanetComponentProps {
-   data?: Planet[]; 
-   planet: Planet;
-}
